39963795fa286626928eb85a4a7c5b1cfc2628e8
NeshoNeshev <69240150+NeshoNeshev@users.noreply.github.com>
2022-11-15 16:05:00 +0200

    Add issue modal

---
+++ Services/BlazorServerTemplate.Services/ServiceModels/IssueInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerTemplate.Services.ServiceModels
+{
+    public class IssueInputModel
+    {
+        [Required]
+        [StringLength(30, ErrorMessage = "Name is too long.")]
+        public string? Name { get; set; }
+
+
+        [Required]
+        [StringLength(10, ErrorMessage = "Date is too long.")]
+        public string? Information { get; set; }
+
+
+        public string? ProjectId { get; set; }
+
+        public string? Author { get; set; }
+    }
+}
